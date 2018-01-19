bb7bdc74db98077406b20d9dcd5b8ef2c02b6f09
Jason Pan <Jason.Pan@team.telstra.com>
2018-01-19 23:55:23 +1100

    Initial commit

---
+++ ios/Sample Project/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Sample Project
+//
+//  Created by Jason Pan on 19/01/2018.
+//  Copyright © 2018 Telstra. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        let jsCodeLocation = URL(string: "http://localhost:8081/index.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+//        let vc = UIViewController()
+        self.view = rootView
+//        self.present(vc, animated: true, completion: nil)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
